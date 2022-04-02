9c7bffea1500da3f6bb587e33d8b9c05e089223e
JiyeonAn <47964927+JiyeonAn@users.noreply.github.com>
2022-04-02 20:13:15 +0900

    Update Lv.신고결과.py

--- Python/Programmers/Lv.신고결과.py
+++ Python/Programmers/Lv.신고결과.py
@@ -1,1 +1,19 @@
 
+def solution(id_list, report, k):
+    answer = []
+    a = list(set(report))
+    dict1 = {name : 0 for name in id_list}
+    dict2 = {name : [] for name in id_list}
+    
+    for i in a:
+        dict2[i.split()[1]].append(i.split()[0])
+        
+    for i in dict2:
+        if len(dict2[i]) >= k:
+            for j in dict2[i]:
+                dict1[j] += 1
+                
+    for i in dict1:
+        answer.append(dict1[i])
+        
+    return answer
